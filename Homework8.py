c2e832ab3bbcbf00324287c8acfb3c352d025b04
DaVonte' Whitfield <47991653+ScytheSoftware@users.noreply.github.com>
2019-04-11 10:25:23 -0400

    Add files via upload

---
+++ Homework8.py
@@ -0,0 +1,20 @@
+import operator
+
+counts = dict()
+bodyOfText = open('EnterTextFileHere.txt', 'r')  #Load a txt file here.
+
+corpus = bodyOfText.read()
+words = corpus.split()  #Turn into an array.
+
+
+for word in words:  # Select each word in the collection of words.
+	counts[word] = counts.get(word, 0) +1
+
+print("")
+print(counts)
+print("")
+
+
+print("The max counted word:'",max(counts.items(), key=operator.itemgetter(1))[0], "'/ Number of times:", counts.get(max(counts.items(), key=operator.itemgetter(1))[0]))
+
+		
